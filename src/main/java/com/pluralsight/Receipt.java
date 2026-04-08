89afae096b4e10cfbbfc3bf0b87eb3e75de89694
pmvroque15 <pmvroque@gmail.com>
2026-04-08 15:18:59 -0400

    added code in the new file

--- src/main/java/com/pluralsight/Receipt.java
+++ src/main/java/com/pluralsight/Receipt.java
@@ -2,6 +2,9 @@ package com.pluralsight;
 
 public class Receipt {
     static void main() {
+        String name = "Bubble Tea";
+        double price = 7.99;
 
+        System.out.print("You bought " + name + "for $" + price);
     }
 }
