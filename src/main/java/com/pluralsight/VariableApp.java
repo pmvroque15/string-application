ac4d90c77e664d40e552534d3bd551c7e1938b2a
pmvroque15 <pmvroque@gmail.com>
2026-04-08 14:55:40 -0400

    Initial Commit and added answers part I & II

---
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,40 @@
+package com.pluralsight;
+
+public class VariableApp {
+
+    static void main() {
+        //        Questions:
+        //        1. Declare a variable to store your favorite color
+        //        2. Declare a variable to store the year you started this class
+        //        3. Declare a variable to store your middle initial
+        //        4. Declare a variable to store whether you have pets (yes/no)
+        //        5. Declare a variable with a nice message or saying
+
+        //        1. Declare AND initialize variables for:
+        //        2. The number of days in a week
+        //        3. The price of coffee ($4.99)
+        //        4. Your favorite letter
+        //        5. Whether it's raining (true/false)
+        //        6. Print all values in complete sentences
+
+        // declare variables here
+        //Part I
+        String favoriteColor;
+        int yearUpSchoolYear;
+        String middleInitial;
+        boolean hasPets;
+        String niceMessage;
+
+        //Part II
+        int numberDaysOfTheWeek = 7;
+        String coffeePrice =  "$4.99";
+        char favoriteLetter = 'P';
+        boolean isRaining = False;
+        // then code solution
+        // then use System.out.println() to display results
+        // ex: System.out.println("The answer is " + answer);
+        // REPEAT FOR NEXT EXERCISE
+
+
+    }
+}
